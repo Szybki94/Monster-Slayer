f138ff4e379222e6e46fe29c195936bfd46fbec7
Kamil Szatkowski <kamil.szatkowski1994@gmail.com>
2024-02-19 22:08:09 +0100

    attack monster button testing

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const app = Vue.createApp({
     data () {
         return {
-            
+            playerHealth: undefined,
+            monsterHealth: undefined,
         };
-    }
+    },
+
+    methods: {
+        attackMonster () {
+            const min = Math.ceil(0);
+            const max = Math.floor(20);
+            const result = Math.floor(Math.random() * (max - min + 1)) + min;
+            console.log(result)
+            return result
+        }
+    },
 })
 
 app.mount('#game')
